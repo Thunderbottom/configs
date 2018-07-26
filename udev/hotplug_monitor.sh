d88c4b88219fa4fda4fe05f89313bebf53653d1d
Chinmay Pai <chinmaydpai@gmail.com>
2018-07-26 13:52:19 +0530

    udev: add udev monitor hotplug rule
    Signed-off-by: Chinmay Pai <chinmaydpai@gmail.com>

---
+++ udev/hotplug_monitor.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/smolboye/.Xauthority
+
+function connect(){
+	xrandr --output DP-3 --off --output DP-2 --off --output DP-1 --off --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --mode 2560x1080_52.00 --pos 0x0 --rotate normal --output LVDS-1 --primary --mode 1280x800 --pos 640x1080 --rotate normal --gamma 0.85:0.85:0.85 --brightness 0.9 --output VGA-1 --off
+}
+
+function disconnect(){
+      xrandr --output HDMI-1 --off
+}
+
+xrandr | grep "HDMI-1 connected" &> /dev/null && connect || disconnect
